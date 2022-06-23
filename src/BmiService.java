2e84be97656df8de8f97c7a11121fb815e13d5e7
Kirill Gureev <kir.gureev2010@ya.ru>
2022-06-23 19:22:00 +0300

    second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,6 @@
 public class BmiService {
-    public double calculate(double weight, double heightSquared) {
+    public double calculate(double weight, double height) {
+        double heightSquared = height * height;
         double bmi = weight / heightSquared;
         return bmi;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 80.7;
-        double heightSquared = 3.24;
-        double result = service.calculate(weight, heightSquared);
+        double weight = 75.1;
+        double height = 1.85;
+        double result = service.calculate(weight, height);
         System.out.println(result);
     }
 }
